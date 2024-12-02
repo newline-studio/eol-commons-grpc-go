dba57b610e3d48dbaf4e1fd898617a62a4698a3a
Jannik Schäfer <me@jschaefer.io>
2024-12-02 14:17:23 +0100

    feat: add main middlewares in streaming version too

--- middleware.go
+++ middleware.go
@@ -11,34 +11,59 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func MiddlewareLogger(logger *slog.Logger, filteredServers ...any) grpc.UnaryServerInterceptor {
+func handleLogging(logger *slog.Logger, filteredServers ...any) func(srv any, handler func() error) error {
 	filterLookup := make(map[any]struct{})
 	for _, srv := range filteredServers {
 		filterLookup[srv] = struct{}{}
 	}
 
-	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-		if _, ok := filterLookup[info.Server]; ok {
-			return handler(ctx, req)
+	return func(srv any, handler func() error) error {
+		if _, ok := filterLookup[srv]; ok {
+			return handler()
 		}
 		start := time.Now()
-		data, err := handler(ctx, req)
+		err := handler()
 		respStatus := status.New(codes.OK, "ok")
 		if err != nil {
 			respStatus, _ = status.FromError(err)
 		}
 		logger.Info(
 			"handle rpc",
-			"method", info.FullMethod,
+			"method", srv,
 			"code", respStatus.Code().String(),
 			"duration", time.Since(start).String(),
 		)
+		return err
+	}
+}
+
+func UnaryMiddlewareLogger(logger *slog.Logger, filteredServers ...any) grpc.UnaryServerInterceptor {
+	loggingHandler := handleLogging(logger, filteredServers...)
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+		var data any
+		err := loggingHandler(info.Server, func() error {
+			handlerRes, err := handler(ctx, req)
+			data = handlerRes
+			if err != nil {
+				return err
+			}
+			return nil
+		})
 		return data, err
 	}
 }
 
-func MiddlewareRecover(logger *slog.Logger) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+func StreamMiddlewareLogger(logger *slog.Logger, filteredServers ...any) grpc.StreamServerInterceptor {
+	loggingHandler := handleLogging(logger, filteredServers...)
+	return func(srv any, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		return loggingHandler(srv, func() error {
+			return handler(srv, ss)
+		})
+	}
+}
+
+func handleRecover(logger *slog.Logger) func(handler func() error) error {
+	return func(handler func() error) error {
 		defer func() {
 			if r := recover(); r != nil {
 				logger.Error(
@@ -46,9 +71,33 @@ func MiddlewareRecover(logger *slog.Logger) grpc.UnaryServerInterceptor {
 					"error", r,
 					"stack", string(debug.Stack()),
 				)
-				err = status.Errorf(codes.Internal, "an unexpected error occurred")
 			}
 		}()
-		return handler(ctx, req)
+		return handler()
+	}
+}
+
+func UnaryMiddlewareRecover(logger *slog.Logger) grpc.UnaryServerInterceptor {
+	recoverHandler := handleRecover(logger)
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+		var data any
+		err := recoverHandler(func() error {
+			handlerRes, err := handler(ctx, req)
+			data = handlerRes
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+		return data, err
+	}
+}
+
+func StreamMiddlewareRecover(logger *slog.Logger) grpc.StreamServerInterceptor {
+	recoverHandler := handleRecover(logger)
+	return func(srv any, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		return recoverHandler(func() error {
+			return handler(srv, ss)
+		})
 	}
 }
